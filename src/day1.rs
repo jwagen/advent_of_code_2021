538e6d60d76fab64dfd62da058549d9513c722af
Johannes Wågen <johannes@wagen.no>
2021-12-01 21:41:56 +0100

    Fix formating

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,15 @@
-pub fn run (input: Vec<u32>) {
+pub fn run(input: Vec<u32>) {
     // Open file
 
-    let val =|n| { input.windows(n).map(|v| v.iter().sum::<u32>()).collect::<Vec<u32>>().windows(2).filter(|v| v[0] < v[1]).count()};
+    let val = |n| {
+        input
+            .windows(n)
+            .map(|v| v.iter().sum::<u32>())
+            .collect::<Vec<u32>>()
+            .windows(2)
+            .filter(|v| v[0] < v[1])
+            .count()
+    };
     println!("Increasing samples window sise 1: {:?}", val(1));
     println!("Increasing samples window sise 3: {:?}", val(3));
-
 }
